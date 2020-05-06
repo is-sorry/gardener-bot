75f92efb6c36f1c078dcc2b54b456b5ea4cb3b0b
syyla <silvia.panaite@gmail.com>
2020-05-06 07:54:41 +0300

    party member message tracking

--- src/main/java/com/syylacodes/gardener/bot/Listener.java
+++ src/main/java/com/syylacodes/gardener/bot/Listener.java
@@ -3,7 +3,6 @@ package com.syylacodes.gardener.bot;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -12,7 +11,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
-import java.util.EnumSet;
 import java.util.HashMap;
 
 @Slf4j
@@ -35,14 +33,13 @@ public class Listener extends ListenerAdapter {
                                     guild
                                             .createTextChannel("Room " + i)
                                             .setParent(category)
-                                            .addPermissionOverride(guild.getPublicRole(), null, EnumSet.of(Permission.VIEW_CHANNEL))
                                             .queue();
 
                             }
                     );
 
         guild.getCategoriesByName("watering rooms", true).get(0)
-                .getChannels().forEach(guildChannel -> bot.getRooms().add(new Room(guildChannel.getIdLong())));
+                .getChannels().forEach(guildChannel -> bot.getRooms().put(guildChannel.getIdLong(), new Room(guildChannel.getIdLong())));
     }
 
 
--- src/main/java/com/syylacodes/gardener/bot/Party.java
+++ src/main/java/com/syylacodes/gardener/bot/Party.java
@@ -3,13 +3,18 @@ package com.syylacodes.gardener.bot;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 @Data
 @NoArgsConstructor
@@ -19,8 +24,10 @@ public class Party {
     private List<Long> guestsId = new ArrayList<>();
     private String message;
     private Long botMessage;
+    private Long invokeChannel;
     private Bot bot;
     private Room room;
+
 
 
     public Party(Bot bot, Long hostId, String message, Room room) {
@@ -48,10 +55,55 @@ public class Party {
         Guild guild = bot.getJda().getGuilds().get(0);
         TextChannel channel = guild.getTextChannelById(room.getId());
         Member member = guild.getMemberById(userId);
-        try {
-            channel.createPermissionOverride(member).setAllow(Permission.VIEW_CHANNEL).queue();
-        } catch (Exception ignored) {
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setColor(Color.GREEN).setDescription("Hey there " + member.getAsMention());
+        channel.sendMessage("Hey there " + member.getAsMention()).queue();
+        if (!guestsId.isEmpty())
+            refreshPartyMessage();
 
+    }
+
+    public void sendPartyMessage(String message, MessageReceivedEvent event, Party party) throws ExecutionException, InterruptedException {
+        Guild guild = event.getGuild();
+        MessageChannel channel = event.getChannel();
+        EmbedBuilder eb = new EmbedBuilder();
+        message = message.trim();
+        eb.setTitle(message.split(" ")[0]);
+        eb.setDescription(message.substring(message.indexOf(" ")));
+        eb.setColor(Color.GREEN);
+        eb.addField("Host: ", guild.getMemberById(hostId).getAsMention(), true);
+        eb.addField("Guests: ", " ", true);
+
+        channel.sendMessage(eb.build()).queue(message1 -> {
+            party.setBotMessage(message1.getIdLong());
+            party.setInvokeChannel(message1.getChannel().getIdLong());
+            message1.addReaction(Objects.requireNonNull(message1.getGuild().getEmoteById(bot.getConfig().getEmoji()))).queue();
+
+        });
+
+    }
+
+    public void refreshPartyMessage() {
+        TextChannel textChannel = bot.getJda().getTextChannelById(invokeChannel);
+
+        EmbedBuilder eb = new EmbedBuilder();
+        message = message.trim();
+        eb.setTitle(message.split(" ")[0]);
+        eb.setDescription(message.substring(message.indexOf(" ")));
+        eb.setColor(Color.GREEN);
+
+        eb.addField("Host: ", textChannel.getGuild().getMemberById(hostId).getAsMention(), true);
+        StringBuilder guests = new StringBuilder();
+        for (Long guest : guestsId) {
+            guests.append(textChannel.getGuild().getMemberById(guest).getAsMention()).append(" ");
         }
+        eb.addField("Guests: ", guests.toString(), true);
+
+        textChannel.editMessageById(botMessage, eb.build()).queue();
+
+    }
+
+    public String toString() {
+        return hostId + " " + guestsId.toString();
     }
 }
